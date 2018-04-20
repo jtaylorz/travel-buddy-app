798636afcfd45bcbc4517a5a4086e3bd514285c5
Wen <jawen@w16452.davidson.edu>
2018-04-19 20:38:43 -0400

    first test

--- LoginWindow/LoginWindow/AppDelegate.swift
+++ LoginWindow/LoginWindow/AppDelegate.swift
@@ -12,7 +12,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    //Test comment to show how git works
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
